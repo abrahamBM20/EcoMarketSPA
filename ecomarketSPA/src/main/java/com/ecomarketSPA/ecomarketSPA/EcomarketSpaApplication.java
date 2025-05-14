3609e616da1b8e0d20252cc4c127d2d31f3b3717
abrahamBM20 <abr.barrera@duocuc.cl>
2025-05-14 18:04:22 -0400

    Add files via upload
    proyecto vacio, solo con carpetas de repositorio, controlador, modelo y servicio.

---
+++ ecomarketSPA/src/main/java/com/ecomarketSPA/ecomarketSPA/EcomarketSpaApplication.java
@@ -0,0 +1,13 @@
+package com.ecomarketSPA.ecomarketSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketSpaApplication.class, args);
+	}
+
+}
---
+++ ecomarketSPA/src/test/java/com/ecomarketSPA/ecomarketSPA/EcomarketSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarketSPA.ecomarketSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomarketSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
